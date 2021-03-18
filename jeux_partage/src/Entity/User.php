0e452e005dfce8c17ba9b20f667ced781f997f8c
Aurélie Gilet <aur.gilet@gmail.com>
2021-03-18 14:30:10 +0100

    correction du getRole de l'entity User

--- jeux_partage/src/Entity/User.php
+++ jeux_partage/src/Entity/User.php
@@ -175,13 +175,9 @@ class User
         return $this;
     }
 
-    public function getRole(): ?array
+    public function getRole()
     {
-		$roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = "ROLE_USER";
-
-        return array_unique($roles);
+		return $this->role;
     }
 
     public function setRole(array $role): self
