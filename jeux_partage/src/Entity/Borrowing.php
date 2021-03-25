4b07a68c4499e3f9912c14bc9fcf7b500208befd
Aurélie Gilet <aur.gilet@gmail.com>
2021-03-25 23:54:12 +0100

    ajout fonctionnalité emprunt + modification affichage dispo sur catalogue

--- jeux_partage/src/Entity/Borrowing.php
+++ jeux_partage/src/Entity/Borrowing.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\BorrowingRepository;
+use App\Entity\Game;
+use App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\BorrowingRepository;
 
 /**
  * @ORM\Entity(repositoryClass=BorrowingRepository::class)
