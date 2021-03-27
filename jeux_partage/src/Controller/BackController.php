c427a8958e1f1fb95b17ffd573f1821d8c1ae990
Celine T <titiceline78@hotmail.fr>
2021-03-27 02:44:16 +0100

    accueil backoffice + gestion des membres

--- jeux_partage/src/Controller/BackController.php
+++ jeux_partage/src/Controller/BackController.php
@@ -2,32 +2,77 @@
 
 namespace App\Controller;
 
-use App\Entity\Game;
 use App\Entity\User;
-use App\Entity\Borrowing;
-use App\Entity\Category;
-use App\Form\GameFormType;
-use App\Form\BorrowingFormType;
-use App\Form\CategoryFormType;
-use App\Repository\GameRepository;
 use App\Repository\UserRepository;
-use App\Repository\BorrowingRepository;
-use App\Repository\CategoryRepository;
+use App\Form\AdminRegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BackController extends AbstractController
 {
     /**
-     * @Route("/back", name="back")
+     * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-        return $this->render('back/index.html.twig', [
-            'controller_name' => 'BackController',
+        return $this->render('back/index.html.twig');
+    }
+
+    
+    /**
+     * @Route("/admin/users", name="admin_users")
+     * @Route("/admin/user/{id}/delete", name="admin_delete_user")
+     */
+    public function adminUsers(UserRepository $repoUsers, EntityManagerInterface $manager, User $user = null): Response
+    {
+        $colonnes = $manager->getClassMetadata(User::class)->getFieldNames();
+
+        $users = $repoUsers->findAll();
+
+        if($user)
+        {
+            $nomUtilisateur = $user->getUsername();
+
+            $manager->remove($user);
+            $manager->flush();
+
+            $this->addFlash("success", "Le membre " . $nomUtilisateur . " a bien été supprimé");
+
+            return $this->redirectToRoute("admin_users");
+        }
+
+        return $this->render("back/admin_users.html.twig", [
+            "colonnes" => $colonnes,
+            "users" => $users
         ]);
     }
+
+    
+    /**
+     * @Route("/admin/user/{id}/edit", name="admin_edit_user")
+     */
+    public function editUser(Request $request, EntityManagerInterface $manager, User $user): Response
+    {
+        $formUser = $this->createForm(AdminRegistrationFormType::class, $user);
+        
+        $formUser->handleRequest($request);
+
+        if($formUser->isSubmitted() && $formUser->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash("success", "Le membre" . $user->getUsername() . " a bien été modifié");
+
+            return $this->redirectToRoute("admin_users");
+        }
+
+        return $this->render("back/admin_edit_user.html.twig", [
+            "formUser" => $formUser->createView()
+        ]);
+    }
+
 }
